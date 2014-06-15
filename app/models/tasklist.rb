6024b502e015df95340f7b7a6fe5b634c3218f35
Maimer <nesnl23@yahoo.com>
2014-06-15 14:00:17 -0400

    Added has_many relationship for users and tasklists tables for usertasklists

--- app/models/tasklist.rb
+++ app/models/tasklist.rb
@@ -1,5 +1,6 @@
 class TaskList < ActiveRecord::Base
   has_many :tasks
+  has_many :usertasklists
   has_many :users, through: :usertasklists
 
   validates :name, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   has_many :tasks
+  has_many :usertasklists
   has_many :tasklists, through: :usertasklists
 
   validates :email, presence: true
